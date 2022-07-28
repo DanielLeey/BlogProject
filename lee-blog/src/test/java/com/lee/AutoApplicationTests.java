4a7b5250bf8b730717ad98cfceb9b25661a9cdd9
Lee <444300917@qq.com>
2022-07-28 19:54:55 +0800

    20220728|office commit

--- lee-blog/src/test/java/com/lee/AutoApplicationTests.java
+++ lee-blog/src/test/java/com/lee/AutoApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.lee.domain.entity.Resource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.io.File;
 import java.util.List;
 
 @SpringBootTest
@@ -40,5 +41,47 @@ public class AutoApplicationTests {
 
         final boolean matches = passwordEncoder.matches("123456", password);
         System.out.println(matches);
+    }
+
+    @Test
+    public void renameDir() {
+
+    }
+
+    public static void changeFileName(String path) {
+        File folder = new File(path);
+        if (folder.exists()) {
+            File[] fileArr = folder.listFiles();
+            if (null == fileArr || fileArr.length == 0) {
+                System.out.println("文件夹是空的!");
+                return;
+            } else {
+                for (File file : fileArr) {
+                    if (file.isDirectory()) {//是文件夹，继续递归，如果需要重命名文件夹，这里可以做处理
+                        System.out.println("文件夹:" + file.getAbsolutePath() + "，继续递归！");
+                        changeFileName(file.getAbsolutePath());
+                    } else {
+                        //是文件，判断是否需要重命名
+                        File parentPath = new File("");//文件所在父级路径
+                        parentPath = file.getParentFile();
+                        String fileName = file.getName(); //旧名
+                        // 获取文件名前半部分（车牌号）
+                        String firstName = fileName.substring(0, fileName.indexOf("("));
+                        // 获取文件名前后部分（检测时间）
+                        String sedName = fileName.substring(fileName.indexOf("(") + 1, fileName.indexOf(")"));
+                        //重新定义名字
+                        String newName = sedName + "(" + firstName + ")" + ".docx";
+                        File newFile = new File(parentPath + "/", newName);
+                        System.out.println(fileName + "-->" + newName);
+                        //进行重命名操作
+                        if (file.renameTo(newFile)) {
+                            System.out.println(file.getName() + "重命名成功---");
+                        } else {
+                            System.out.println(file.getName() + "重命名失败+++");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -37,3 +37,90 @@ DaoAuthenticationProvider additionalAuthenticationChecks()对比传入的present
 	}
 ~~~~
 
+
+
+## 认证
+
+
+
+
+
+## 授权
+
+
+
+
+
+## 改进
+
+### 1.问题一
+
+问题描述：
+
+登录时，先通过UserDetailsService实现类查询数据库用户信息封装为UserDetails对象返回，如果多次密码错误会导致重复查询数据库。
+
+解决方案：
+
+将UserDetailsService实现类查询数据库用户信息，存入redis中，重复登录时从redis先查询。
+
+将登录service中，通过认证后获取的UserDetails存入redis，这一步骤删除
+
+
+
+### 2.问题二
+
+问题描述：
+
+如果用户密码修改，redis中保存的还是原始密码，未更新，传入的正确密码匹配不上
+
+解决方案：
+
+修改密码之后删除redis中的UserDetails对象
+
+同理角色修改，资源类型修改都需要删除redis中缓存
+
+~~~~java
+/**
+ * 后台用户缓存操作类
+ */
+public interface UserCacheService {
+    /**
+     * 删除后台用户缓存
+     */
+    void delUser(Long userId);
+
+    /**
+     * 删除后台用户资源列表缓存
+     */
+    void delResourceList(Long userId);
+
+    /**
+     * 当角色相关资源信息改变时删除相关后台用户缓存
+     */
+    void delResourceListByRole(Long roleId);
+
+    /**
+     * 当角色相关资源信息改变时删除相关后台用户缓存
+     */
+    void delResourceListByRoleIds(List<Long> roleIds);
+
+    /**
+     * 当资源信息改变时，删除资源项目后台用户缓存
+     */
+    void delResourceListByResource(Long resourceId);
+}
+~~~~
+
+
+
+### 3.问题三
+
+问题描述：
+
+如果redis宕机，会导致登录时从redis获取UserDetails对象直接报错，无法登录
+
+解决方案：
+
+通过AOP处理缓存操作异常
+
+要保证缓存业务类中的方法执行不影响正常的业务逻辑，就需要在所有方法中添加`try catch`逻辑。使用AOP，我们可以在一个地方写上`try catch`逻辑，然后应用到所有方法上去。
